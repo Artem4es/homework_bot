73131eb14c070466fd1998fb897bff334620a46a
Артём Ежов <dragster1001@yandex.ru>
2022-12-11 21:45:15 +0300

    Homework 1.5

--- homework.py
+++ homework.py
@@ -40,8 +40,10 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_PERIOD = int(os.getenv('RETRY_PERIOD'))
-ENDPOINT = os.getenv('ENDPOINT')
+RETRY_PERIOD = int(os.getenv('RETRY_PERIOD', 600))
+ENDPOINT = os.getenv(
+    'ENDPOINT', 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+)
 
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
